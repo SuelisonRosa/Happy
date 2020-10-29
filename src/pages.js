6a7a3727ae36855828b9d7c5a4222b97754aa874
Suélison <suelisonrosa5@gmail.com>
2020-10-29 08:58:57 -0300

    Update pages.js

--- src/pages.js
+++ src/pages.js
@@ -4,18 +4,15 @@ module.exports = {
         return res.render('index')
     },
 
-dayCareCenter(req, res){
+dayCareCenter(req, res) {
     return res.render('day-care-center')
-    
 },
 
-dayCareCenters(req, res){
+dayCareCenters(req, res) {
     return res.render('day-care-centers')
-
 },
 
-createDayCareCenter(req, res){
+createDayCareCenter(req, res) {
     return res.render('create-day-care-center')
-}
-
+    }
 }
